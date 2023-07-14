323e8a51e6391fb843a1f8e1dbe4bd2c92953264
Choi Seongwon <vlflalektzja@naver.com>
2023-07-14 14:53:44 +0900

    Init

---
+++ src/main/java/com/mcttekingstouch/mcttekingstouch/McTteKingsTouchApplication.java
@@ -0,0 +1,13 @@
+package com.mcttekingstouch.mcttekingstouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McTteKingsTouchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McTteKingsTouchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mcttekingstouch/mcttekingstouch/McTteKingsTouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcttekingstouch.mcttekingstouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McTteKingsTouchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
