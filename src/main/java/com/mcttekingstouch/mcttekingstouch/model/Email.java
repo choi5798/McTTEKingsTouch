394d6031b3e05d494fc7bd99e249268db6fd726b
Choi Seongwon <vlflalektzja@naver.com>
2023-07-17 22:05:19 +0900

    feat: Email 객체 생성자 구현, 이메일 형식 검증 정규표현식 적용

--- src/main/java/com/mcttekingstouch/mcttekingstouch/model/Email.java
+++ src/main/java/com/mcttekingstouch/mcttekingstouch/model/Email.java
@@ -1,5 +1,18 @@
 package com.mcttekingstouch.mcttekingstouch.model;
 
 public class Email {
-    private String address;
+    private final String address;
+
+    public Email(String address) {
+        validate(address);
+        this.address = address;
+    }
+
+    private void validate(String address) {
+        if (!address.matches("\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b")) {
+            throw new IllegalArgumentException("올바르지 않은 이메일 형식입니다");
+        }
+    }
+
+
 }
