f7ffe6310b7018c9a1cc4ffbaa6ac724c9c3faed
Choi Seongwon <vlflalektzja@naver.com>
2023-07-18 18:31:23 +0900

    feat: 버거, 주문 엔티티 클래스 타입 변경 record -> class, setter 추가

--- src/main/java/com/mcttekingstouch/mcttekingstouch/model/Burger.java
+++ src/main/java/com/mcttekingstouch/mcttekingstouch/model/Burger.java
@@ -3,13 +3,82 @@ package com.mcttekingstouch.mcttekingstouch.model;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record Burger(
-        UUID burgerId,
-        String burgerName,
-        long price,
-        BurgerType burgerType,
-        BurgerCompany burgerCompany,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
-) {
+public class Burger {
+    private final UUID burgerId;
+    private String burgerName;
+    private Long price;
+    private BurgerType burgerType;
+    private BurgerCompany burgerCompany;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Burger(
+            UUID burgerId,
+            String burgerName,
+            long price,
+            BurgerType burgerType,
+            BurgerCompany burgerCompany,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt
+    ) {
+        this.burgerId = burgerId;
+        this.burgerName = burgerName;
+        this.price = price;
+        this.burgerType = burgerType;
+        this.burgerCompany = burgerCompany;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getBurgerId() {
+        return burgerId;
+    }
+
+    public String getBurgerName() {
+        return burgerName;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public BurgerType getBurgerType() {
+        return burgerType;
+    }
+
+    public BurgerCompany getBurgerCompany() {
+        return burgerCompany;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setBurgerName(String burgerName) {
+        this.burgerName = burgerName;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public void setBurgerType(BurgerType burgerType) {
+        this.burgerType = burgerType;
+    }
+
+    public void setBurgerCompany(BurgerCompany burgerCompany) {
+        this.burgerCompany = burgerCompany;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- src/main/java/com/mcttekingstouch/mcttekingstouch/model/Order.java
+++ src/main/java/com/mcttekingstouch/mcttekingstouch/model/Order.java
@@ -1,15 +1,112 @@
 package com.mcttekingstouch.mcttekingstouch.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-public record Order(
-        UUID orderId,
-        Email email,
-        String address,
-        String postcode,
-        OrderStatus status,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
-) {
+public class Order {
+    private final UUID orderId;
+    private final Email email;
+    private String address;
+    private String postcode;
+    private OrderStatus status;
+    @JsonIgnore
+    private List<OrderItems> orderItems;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // AllArgsConstructor
+    public Order(
+            UUID orderId,
+            Email email,
+            String address,
+            String postcode,
+            OrderStatus status,
+            List<OrderItems> orderItems,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt) {
+        this.orderId = orderId;
+        this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+        this.status = status;
+        this.orderItems = orderItems;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // orderItems 없는 생성자
+    public Order(UUID orderId,
+                 Email email,
+                 String address,
+                 String postcode,
+                 OrderStatus status,
+                 LocalDateTime createdAt,
+                 LocalDateTime updatedAt) {
+        this.orderId = orderId;
+        this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public List<OrderItems> getOrderItems() {
+        return orderItems;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setOrderItems(List<OrderItems> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
